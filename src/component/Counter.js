338cf48bf6c756c47e7f14bacd4e90b648214e6d
Prajun Trital <prajuncs@gmail.com>
2022-03-07 18:46:31 -0600

    added comments in counter

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -3,7 +3,9 @@ import React , { useState } from 'react'
 function Counter() {
     const[counter, setCounter] = useState(0);
 
-    const increment = () =>{
+    //Always get the prevCounter and then increment it
+    //Here the setCounter is taking a function
+    const increment = () => {
         return setCounter(prevCounter => prevCounter + 1);
     }
 
@@ -11,12 +13,13 @@ function Counter() {
         return setCounter(prevCounter => prevCounter - 1);
     }
 
-
   return (
     <>
         <div>{counter}</div>
     <button onClick = {increment} > Increment </button>
     <button onClick = {decrement} > Decrement </button>
+
+    {/* I am creating a function in this button itself */}
     <button onClick = {() => setCounter(prevCounter => prevCounter/2)} > Divide By 2 </button>
 
     </>
